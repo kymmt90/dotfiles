7da10e7a9dca8df53b29b0a6a07973df729658b7
K. Yamamoto <kymmt90@gmail.com>
2015-03-29 12:32:58 +0900

    Introduce Cask

---
+++ Cask
@@ -0,0 +1,21 @@
+(source gnu)
+(source melpa)
+
+(depends-on "color-theme")
+
+(depends-on "color-moccur")
+(depends-on "redo+")
+(depends-on "migemo")
+(depends-on "auto-complete")
+(depends-on "popup")
+(depends-on "yasnippet")
+
+(depends-on "helm")
+(depends-on "helm-migemo")
+
+(depends-on "haskell-mode")
+(depends-on "io-mode")
+(depends-on "markdown-mode")
+(depends-on "python-mode")
+(depends-on "scala-mode2")
+
--- init.el
+++ init.el
@@ -1,31 +1,6 @@
-; define a function to add arguments to "load-path"
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-	      (expand-file-name (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-            (normal-top-level-add-subdirs-to-load-path))))))
-
-; add directories recursively to load-path.
-(add-to-load-path "elisp" "elpa" "conf" "public_repos")
-
-; package.el
-(when (require 'package nil t)
-  (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives
-			   '("ELPA" . "http://tromey.com/elpa/"))
-  (add-to-list 'package-archives
-			   '("melpa" . "http://melpa.milkbox.net/packages/"))
-  (package-initialize))
-
-; auto-install.el
-(when (require 'auto-install nil t)
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  (auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup))
+; Cask
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 
 
 ; save history of buffers
