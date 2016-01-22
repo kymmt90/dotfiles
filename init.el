8046c8aaa7d8248f7c76564bc5ff007626760ad3
Kohei Yamamoto <kymmt90@gmail.com>
2016-01-22 22:47:41 +0900

    Tweak migemo config

--- init.el
+++ init.el
@@ -168,7 +168,7 @@
   (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
-  (setq migemo-coding-system 'utf-8-unix)
+  (setq migemo-coding-system 'utf-8)
   (load-library "migemo")
   (migemo-init)
 )
