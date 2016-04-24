a62e1286e23bf98eab1ac898fbf78b23ec3526fa
Kohei Yamamoto <kymmt90@gmail.com>
2016-04-24 21:15:07 +0900

    Use 2 width whitespace indent in web-mode

--- init.el
+++ init.el
@@ -269,9 +269,10 @@
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)))
 
 (defun my-web-mode-hook()
+  (setq-default indent-tabs-mode nil)
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-script-indent-offset 2))
+  (setq web-mode-code-indent-offset 2))
 (add-hook 'web-mode-hook 'my-web-mode-hook)
 
 ;; ruby-mode
