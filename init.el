ecbae3d2adef0e744145542e2068c9daa9e618b3
Kohei Yamamoto <kymmt90@gmail.com>
2016-11-14 23:35:51 +0900

    [init.el] Make Ruby linters disabled

--- init.el
+++ init.el
@@ -292,10 +292,7 @@
 
 ;; ruby-mode
 (setq ruby-insert-encoding-magic-comment nil)
-(add-hook 'ruby-mode-hook '(lambda ()
-							 (setq flycheck-checker 'ruby-rubocop)
-							 (flycheck-mode 1)))
-
+(setq-default flycheck-disabled-checkers '(ruby ruby-rubylint ruby-rubocop))
 (add-hook 'ruby-mode-hook 'robe-mode)
 (autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
 (autoload 'ac-robe-setup "ac-robe" "auto-complete robe" nil nil)
