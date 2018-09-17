c16f9d07be24ba31953716506cff12b42276df00
Kohei Yamamoto <kymmt90@gmail.com>
2018-09-17 14:44:28 +0900

    [fish] rbenv initialization

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,5 @@ set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showcolorhints 'yes'
+
+rbenv init - | source
