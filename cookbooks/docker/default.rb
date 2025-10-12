ae8fa3c6aa0fd003bfb9883d47174c4124fb3a77
Kohei Yamamoto <kymmt90@gmail.com>
2025-10-12 11:42:44 +0900

    Fix to set docker credsStore for colima

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -7,8 +7,19 @@ directory File.join(ENV['HOME'], '.docker') do
   mode '755'
 end
 
+config_path = File.join(ENV['HOME'], '.docker', 'config.json')
+
+execute 'configure credsStore' do
+  tmp_path = '/tmp/docker-config.json'
+
+  command <<~CMD
+    cat #{config_path} | jq '.credsStore = "osxkeychain"' > #{tmp_path} && mv #{tmp_path} #{config_path}
+  CMD
+
+  not_if %(cat #{config_path} | jq -e '.credsStore == "osxkeychain"')
+end
+
 execute "configure cliPluginsExtraDirs" do
-  config_path = File.join(ENV['HOME'], '.docker', 'config.json')
   config = <<~CONFIG
     {"cliPluginsExtraDirs":["#{`brew --prefix`.strip}/lib/docker/cli-plugins"]}
   CONFIG
