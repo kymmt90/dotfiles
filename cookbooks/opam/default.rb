9c73927f3a6efc93610ff11622648845c7a56bd3
Kohei Yamamoto <kymmt90@gmail.com>
2026-01-12 13:07:49 +0900

    Install opam packages for default switch

--- cookbooks/opam/default.rb
+++ cookbooks/opam/default.rb
@@ -5,6 +5,20 @@ end
 
 package "opam"
 
+execute "opam init -y" do
+  not_if "test -d #{opam_config_directory_path}/opam-init"
+end
+
+packages = %w[
+  ocaml-lsp-server
+  ocamlformat
+  odoc
+  utop
+].join(" ")
+execute "opam install --switch default -y #{packages}" do
+  not_if "opam install --switch default --check #{packages} >/dev/null 2>&1"
+end
+
 # utop key bindings
 # cf. https://discuss.ocaml.org/t/default-key-bindings-for-utop/3950
 config ".lambda-term-inputrc" do
