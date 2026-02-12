a1bafac1dbeaafd5d1c24841c21469787042b37d
Kohei Yamamoto <kymmt90@gmail.com>
2026-02-12 22:37:52 +0900

    Accept non-XDG compliant dot-prefixed directory names

--- roles/helper.rb
+++ roles/helper.rb
@@ -20,7 +20,8 @@ define :config_directory, xdg: true do
   directories = params[:name]
   next if directories == "."
 
-  directories = File.join(XDG_CONFIG_HOME, directories) if params[:xdg]
+  base_dir = params[:xdg] ? XDG_CONFIG_HOME : ENV["HOME"]
+  directories = File.join(base_dir, directories)
   directory directories do
     user node[:user]
   end
@@ -33,7 +34,14 @@ define :config, tool: nil, xdg: true do
     xdg xdg
   end
 
-  tool_name = params[:tool] || directories.split("/")[1]
+  tool_name = params[:tool]
+
+  # if params[:xdg] is true, tool name is the first directory name in XDG_CONFIG_HOME.
+  # otherwise, tool name is the first directory name in HOME without dot prefix.
+  tool_name ||= params[:xdg] ?
+    directories.split("/")[1] :
+    directories.split("/")[0].delete_prefix(".")
+
   config_src = params[:name]
   config_src = File.join(File.basename(XDG_CONFIG_HOME), config_src) if params[:xdg]
   config_src = File.join("cookbooks", tool_name, "files", config_src)
