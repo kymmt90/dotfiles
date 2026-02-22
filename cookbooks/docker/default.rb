eb000290d674a8a9d2aefdcd91e3fe5909bc5a0b
Kohei Yamamoto <kymmt90@gmail.com>
2026-02-22 13:46:10 +0900

    Define docker_config to encapsulate custimization details

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -7,31 +7,35 @@ config_directory ".docker" do
   xdg false
 end
 
-docker_config_path = File.join(ENV["HOME"], ".docker", "config.json")
+define :docker_config do
+  config_path = File.join(ENV["HOME"], ".docker", params[:name])
 
-execute "credsStore configuration" do
-  tmp_path = "/tmp/docker-config.json"
+  execute "credsStore configuration" do
+    tmp_path = "/tmp/docker-config.json"
 
-  command <<~CMD
-    cat #{docker_config_path} | \
-    jq '.credsStore = "osxkeychain"' > #{tmp_path} && mv #{tmp_path} #{docker_config_path}
-  CMD
+    command <<~CMD
+      cat #{config_path} | \
+      jq '.credsStore = "osxkeychain"' > #{tmp_path} && mv #{tmp_path} #{config_path}
+    CMD
 
-  not_if %(cat #{docker_config_path} | jq -e '.credsStore == "osxkeychain"')
+    not_if %(cat #{config_path} | jq -e '.credsStore == "osxkeychain"')
+  end
+
+  execute "cliPluginsExtraDirs configuration" do
+    config = <<~CFG
+      {"cliPluginsExtraDirs":["#{`brew --prefix`.strip}/lib/docker/cli-plugins"]}
+    CFG
+    tmp_path = "/tmp/docker-config.json"
+
+    command <<~CMD
+      cat #{config_path} | jq '. + #{config}' > #{tmp_path} && mv #{tmp_path} #{config_path}
+    CMD
+
+    not_if "grep -q cliPluginsExtraDirs #{config_path}"
+  end
 end
 
-execute "cliPluginsExtraDirs configuration" do
-  config = <<~CFG
-    {"cliPluginsExtraDirs":["#{`brew --prefix`.strip}/lib/docker/cli-plugins"]}
-  CFG
-  tmp_path = "/tmp/docker-config.json"
-
-  command <<~CMD
-    cat #{docker_config_path} | jq '. + #{config}' > #{tmp_path} && mv #{tmp_path} #{docker_config_path}
-  CMD
-
-  not_if "grep -q cliPluginsExtraDirs #{docker_config_path}"
-end
+docker_config "config.json"
 
 execute "brew services start colima" do
   only_if <<~COND
